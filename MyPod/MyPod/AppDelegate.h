d222d922f97fb67831d6a59745c047d354510804
jyyc <jyyc@jyycdeMacBook-Pro.local>
2021-11-11 16:27:15 +0800

    add pod product

---
+++ MyPod/MyPod/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyPod
+//
+//  Created by jyyc on 2021/11/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MyPod/MyPod/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MyPod
+//
+//  Created by jyyc on 2021/11/11.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
